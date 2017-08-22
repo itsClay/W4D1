5fd616c7950f47a5e57d5b0d855a485538654902
Clay Shaw <Claydshaw@gmail.com>
2017-08-21 17:39:37 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+First routes app set up for Rails.
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+    if user
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find_by("id = ?", :id)
+    if user.update(params[:user].permit(:name, :email))
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+end
