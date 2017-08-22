223a5b9fd266976bb9c51f38ed74621472946c65
Clay Shaw <Claydshaw@gmail.com>
2017-08-21 17:58:11 -0700

    Add update/delete controller functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find_by("id = ?", :id)
+    user = User.find_by("id = #{params[:id]}")
     if user.update(params[:user].permit(:name, :email))
       render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      redirect_to '/users'
     else
       render json: user.errors.full_messages, status: :unprocessable_entity
     end
